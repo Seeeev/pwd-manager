b789e74cf371636a7a33f4302b5ca01a3558dc79
Seeeev <sevenabante9@gmail.com>
2023-12-24 08:50:11 +0800

    added pwd table

---
+++ src/app/admin/pwd-table.tsx
@@ -0,0 +1,177 @@
+"use client";
+import { useEffect, useState } from "react";
+import { $Enums, Pwd } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import prisma from "@/prisma";
+
+type tableType = {
+  pwdNumber: String;
+  name: String;
+  barangay: number | null;
+  status: $Enums.Status;
+};
+
+const columns: ColumnDef<tableType>[] = [
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const pwd = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={async () =>
+                await prisma?.pwd.update({
+                  where: { pwdNumber: pwd.pwdNumber as string },
+                  data: { status: "approved" },
+                })
+              }
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "pwdNumber",
+    header: "PWD Number",
+  },
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "barangay",
+    header: "Barangay",
+  },
+];
+
+export default function PwdTable() {
+  const [data, setData] = useState<tableType[]>([]);
+  const query = useQuery({
+    queryKey: ["pwd"],
+    queryFn: () =>
+      fetch("api/pwd", {
+        method: "GET",
+      }).then((val) => val.json()),
+  });
+
+  useEffect(() => {
+    if (query.data) {
+      console.log(query.data);
+      query.data.map((val: Pwd) => console.log(val.status));
+      const newData: tableType[] = query.data.map((val: Pwd) => {
+        const pwdNumber = val.pwdNumber;
+        const name = `${val.lastName}, ${val.firstName} ${val.middleName} ${val.suffix}`;
+        const barangay = val.barangayId;
+        const status = val.status;
+        return {
+          barangay: barangay,
+          name: name,
+          pwdNumber: pwdNumber,
+          status: status!,
+        };
+      });
+
+      setData(newData);
+    }
+  }, [query.data]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
