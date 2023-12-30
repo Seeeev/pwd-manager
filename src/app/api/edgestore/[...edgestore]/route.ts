1443825428d0156151bb52639371ace8828e0d9c
Seeeev <sevenabante9@gmail.com>
2023-12-30 21:49:32 +0800

    fixed edgestore required type

--- src/app/api/edgestore/[...edgestore]/route.ts
+++ src/app/api/edgestore/[...edgestore]/route.ts
@@ -14,14 +14,15 @@ const edgeStoreRouter = es.router({
 const handler = createEdgeStoreNextHandler({
   router: edgeStoreRouter,
 });
+
+export type EdgeStoreRouter = typeof edgeStoreRouter;
+
+export const backendClient = initEdgeStoreClient({
+  router: edgeStoreRouter,
+});
  
 export { handler as GET, handler as POST };
  
 /**
  * This type is used to create the type-safe client for the frontend.
  */
-export type EdgeStoreRouter = typeof edgeStoreRouter;
-
-export const backendClient = initEdgeStoreClient({
-  router: edgeStoreRouter,
-});
--- src/lib/edgestore.ts
+++ src/lib/edgestore.ts
@@ -2,8 +2,15 @@
  
 import { type EdgeStoreRouter } from '../app/api/edgestore/[...edgestore]/route';
 import { createEdgeStoreProvider } from '@edgestore/react';
+import { type InferClientResponse } from '@edgestore/server/core';
  
 const { EdgeStoreProvider, useEdgeStore } =
   createEdgeStoreProvider<EdgeStoreRouter>();
  
 export { EdgeStoreProvider, useEdgeStore };
+
+// ...
+/**
+ * This helper type can be used to infer the response type of the backend client
+ */
+export type ClientResponse = InferClientResponse<EdgeStoreRouter>;
