71f8367066c321c0d7abb0aa17034dbb8887dc1d
Seeeev <sevenabante9@gmail.com>
2024-01-16 09:36:42 +0800

    added navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <div className="w-screen h-10 bg-primary flex justify-end items-center px-14">
+      <Link href={"/admin"} className="text-white">
+        {" "}
+        Admin Page
+      </Link>
+    </div>
+  );
+}
