ba4766432ef0518dcd184eab26a87bb6ab24e496
Seeeev <sevenabante9@gmail.com>
2024-01-16 13:51:07 +0800

    aded order by

--- src/app/api/occupation/route.ts
+++ src/app/api/occupation/route.ts
@@ -2,7 +2,11 @@ import prisma from "@/prisma";
 
 export async function GET() {
   console.log('occupation called');
-  const res = await prisma.occupation.findMany();
+  const res = await prisma.occupation.findMany({
+    orderBy: {
+      id: "desc"
+    }
+  });
 
   return Response.json(res);
 }
