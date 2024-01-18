991f0a3eadef66c03cc1f9d28b897d798131973e
Seeeev <sevenabante9@gmail.com>
2024-01-18 10:28:01 +0800

    revert changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,7 @@ export default function NavBar() {
   return (
     <div className="w-screen h-10 bg-primary flex justify-end items-center px-14">
       <Link href={"/admin"} className="text-white">
-        Admin Page1
+        Admin Page
       </Link>
     </div>
   );
